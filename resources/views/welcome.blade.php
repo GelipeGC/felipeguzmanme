157b5ec21684c6e27225bc1da8b00796a40e2551
Felipe Guzman <felipe.lightcone@gmail.com>
2017-11-20 23:53:27 -0600

    landing information personal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,92 +4,36 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Felipe Guzman</title>
+        <link href="{{asset('css/app.css')}}" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css')}}">
 
-        <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+        <div id="wrapper">
+            <section id="main">
+                <header>
+                <span class="avatar"><img src="images/avatar.jpg" alt="" /></span>
+                <h1>Felipe Guzmán</h1>
+                <p>Junior Developer</p>
+                </header>
+                <footer>
+                    <ul class="icons">
+                    <li><a href="https://www.linkedin.com/in/felipe-guzman-cortez-4baa43138/" class="fa fa-linkedin-square" target="_blank">user</a></li>
+                    <li><a href="https://twitter.com/GelipeC" class="fa fa-twitter" target="_blank">Twitter</a></li>
+                    <li><a href="https://github.com/GelipeGC" class="fa fa fa-github" target="_blank">fa fa-github</a></li>
+                    <li><a href="https://bitbucket.org/GelipeGC/" class="fa fa fa-bitbucket" target="_blank">bitbucket</a></li>
+                    </ul>
+                </footer>
+            </section>
+            <!-- Footer -->
+            <footer id="footer">
+                <ul class="copyright">
+                <li>&copy; Felipe Guzmán</li></li>
+                </ul>
+            </footer>
         </div>
     </body>
 </html>
